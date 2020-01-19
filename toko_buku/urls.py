edf2f86e6b457ec7d88ed3248173af63b1884c8a
wahyu <wahyu@istw.co.id>
2020-01-20 04:28:19 +0700

    root / pindah buku

--- toko_buku/urls.py
+++ toko_buku/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('buku/',include('buku.urls')),
+    path('',include('buku.urls')),
     path('admin/', admin.site.urls),
 
 ]
