f839dfcc3050c7b62037601adcc05f4ed737e1f7
wahyu <wahyu@istw.co.id>
2020-01-24 12:52:09 +0700

    views tanpa html ok

--- buku/views.py
+++ buku/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from  django.template import loader
+from .models import Buku
+
+
+def index(request):
+    latest_buku_list = Buku.objects.all()
+    template = loader.get_template('buku/index.html')
+    output = ', '.join([b.judul for b in latest_buku_list])
+    return HttpResponse(output)
--- toko_buku/urls.py
+++ toko_buku/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('buku/',include('buku.urls')),
     path('admin/', admin.site.urls),
+
 ]
