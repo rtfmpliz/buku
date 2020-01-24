4610a28be446cedfadfec8a44115c07e1438e851
wahyu <wahyu@istw.co.id>
2020-01-24 15:32:38 +0700

    views selesai

--- buku/urls.py
+++ buku/urls.py
@@ -3,6 +3,7 @@ from django.urls import  path
 
 from . import views
 
+app_name = 'buku'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:buku_id>/', views.detail, name='detail'),
